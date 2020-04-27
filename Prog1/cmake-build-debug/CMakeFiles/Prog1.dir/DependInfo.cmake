d50828e65374889a2369a4a5155b398e94d59cc4
Jonathan Young <jonathan.p.young@gmail.com>
2020-04-26 18:44:05 -0700

    Initial provided code

---
+++ Prog1/cmake-build-debug/CMakeFiles/Prog1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/zero_/CLionProjects/CSS432/Prog1/Client.cpp" "/cygdrive/c/Users/zero_/CLionProjects/CSS432/Prog1/cmake-build-debug/CMakeFiles/Prog1.dir/Client.cpp.o"
+  "/cygdrive/c/Users/zero_/CLionProjects/CSS432/Prog1/Server.cpp" "/cygdrive/c/Users/zero_/CLionProjects/CSS432/Prog1/cmake-build-debug/CMakeFiles/Prog1.dir/Server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Prog1/cmake-build-debug/CMakeFiles/Prog1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prog1.dir/Client.cpp.o"
+  "CMakeFiles/Prog1.dir/Server.cpp.o"
+  "Prog1.exe"
+  "Prog1.pdb"
+  "libProg1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Prog1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
