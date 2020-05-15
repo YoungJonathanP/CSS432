1f2a75d58ad9ef2c4165545343b931006acb3f31
Jonathan Young <YoungJon@uw.edu>
2020-05-14 23:53:31 -0700

    formatted send and received values between server and client. Shutdown call portioned after last server write

--- Prog3/Client.cpp
+++ Prog3/Client.cpp
@@ -1,6 +1,6 @@
 //
 // Created by Jonathan Young on 5/10/2020.
-// Client code that emulates hw3_demo.cpp
+// Client code that emulates hw3
 //
 
 #include "Socket.h"
@@ -8,40 +8,42 @@
 
 using namespace std;
 
-void client( char ipName[] );
-void usage( char progName[] );
+void client(char ipName[]);
+
+void usage(char progName[]);
 
 Socket *sock;
 
-int main( int argc, char* argv[] ) {
-    if ( argc > 1 ) {
-        sock = new Socket( atoi( argv[1] ) );
-        if (argc == 3){
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        sock = new Socket(atoi(argv[1]));
+        if (argc == 3) {
             client(argv[2]);
         }
     } else {
-        usage( argv[0] );
+        usage(argv[0]);
         return -1;
     }
     return 0;
 }
 
 
-void client( char ipName[] ) {
+void client(char ipName[]) {
 
     // Get a client sd
-    int clientSd = sock->getClientSocket( ipName );
+    int clientSd = sock->getClientSocket(ipName);
 
     // Exchange data
     char message[1500];
-    write( clientSd, message, 1500 );
-    read( clientSd, message, 1 );
+    write(clientSd, message, 10); // send a 10B transmission to server
+    read(clientSd, message, 10); // expect a 10B transmission from server
+    write(clientSd, message, 1450); // send a 1450B transmission to server
 
     // Close socket to send FIN.
-    close( clientSd );
+    close(clientSd);
 }
 
-void usage( char progName[] ) {
+void usage(char progName[]) {
     cerr << "usage:" << endl;
     cerr << "client invocation: " << progName << " ipPort ipName" << endl;
 }
--- Prog3/Server.cpp
+++ Prog3/Server.cpp
@@ -1,6 +1,6 @@
 //
 // Created by Jonathan Young on 5/10/2020.
-// Server code that emulates hw3_demo.cpp
+// Server code that emulates hw3
 //
 
 #include "Socket.h"
@@ -8,45 +8,48 @@
 
 using namespace std;
 
-void server( );
-void usage( char progName[] );
+void server();
+
+void usage(char progName[]);
 
 Socket *sock;
 
 // argument acceptance and validation
-int main( int argc, char* argv[] ) {
-    if ( argc > 1 ) {
-        sock = new Socket( atoi( argv[1] ) );
-        if ( argc == 2 )
-            server( );
-    }
-    else {
-        usage( argv[0] );
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        sock = new Socket(atoi(argv[1]));
+        if (argc == 2)
+            server();
+    } else {
+        usage(argv[0]);
         return -1;
     }
     return 0;
 }
 
 // server code -- must use the <shutdown> system call
-void server( ) {
+void server() {
 
     // Get a server sd
-    int serverSd = sock->getServerSocket( );
+    int serverSd = sock->getServerSocket();
 
     // Exchange data
     char message[1500];
-    read( serverSd, message, 1500 );
-    write( serverSd, message, 1 );
+    read(serverSd, message, 10); // expect a 10B transmission from client
+    write(serverSd, message, 10); // send a 10B transmission to client
 
-    // serverside shutdown call
+    // serverside shutdown call - send FIN call to client
     shutdown(serverSD, SHUT_WR);
 
+    read(serverSd, message, 1450); // expect a 1450B transmission from client
+
+
     // Close socket but not send FIN.
-    close( serverSd );
+    close(serverSd);
 }
 
 
-void usage( char progName[] ) {
+void usage(char progName[]) {
     cerr << "usage:" << endl;
     cerr << "server invocation: " << progName << " ipPort" << endl;
 }
